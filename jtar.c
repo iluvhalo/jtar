1e521e91aeeddc97945aaae0760345c29ff9bc5b
iluvhalo <hgd145@vols.utk.edu>
2018-02-21 18:08:33 -0500

    Adding jtar.c

---
+++ jtar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dllist.h>
+#include <fields.h>
+#include <jval.h>
+#include <jrb.h>
+
+int main (int argc, char **argv) {
+
+	printf("jtar is running\n");
+
+	return 0;
+}
