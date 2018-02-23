f0908be5019a44634bbf443ebaae53ab3a132dd1
iluvhalo <hgd145@vols.utk.edu>
2018-02-22 22:01:58 -0500

    Update jtar.c

--- jtar.c
+++ jtar.c
@@ -7,11 +7,13 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-int process_files(char *s);
+int extractTar(int argc, char **argv, char print);
+int createTar(int argc, char **argv, char print);
+int process_files(char *file);
 
 int main (int argc, char **argv) {
    int i;
-   char gucci;
+   char gucci, print;
 
    if (argc < 2) {
       fprintf(stderr, "usage: jtar [cxv] [files ...]\n");
@@ -27,14 +29,57 @@ int main (int argc, char **argv) {
       exit(1);
    }
 
-   for (i = 2; i < argc; i++) {
-      process_files(argv[i]);
+   print = 0;
+   if ((strcmp(argv[1], "cv") == 0) || (strcmp(argv[1], "xv") == 0)) print = 1;
+
+   if ((strcmp(argv[1], "c") == 0) || (strcmp(argv[1], "cv") == 0)) {
+      printf("command is createTar()\n");
+      createTar(argc, argv, print);
+   } else {
+      printf("command is extractTar()\n");
+      extractTar(argc, argv, print);
    }
 
 	return 0;
 }
 
+int createTar(int argc, char **argv, char print) {
+   int i;
+
+   printf("Argument: %d\n", print);
+
+   for (i = 2; i < argc; i++) {
+      printf("File: %s\n", argv[i]);
+      process_files(argv[i]);
+   }
+}
+
+int extractTar(int argc, char **argv, char print) {
+   int i;
+
+   printf("Argument: %d\n", print);
+
+   for (i = 2; i < argc; i++) {
+      printf("File: %s\n", argv[i]);
+      process_files(argv[i]);
+   }
+}
+
 int process_files(char *s) {
-   printf("File: %s\n", s);
+   struct stat buf;
+   int exists;
+   
+   printf("Processing file: %s\n", s);
+
+   exists = lstat(s, &buf);
+
+   if (exists < 0) {
+      printf("%s does not exist\n", s);
+      return -1;
+   } else {
+      if (S_ISDIR(buf.st_mode)) {
+         printf("is a directory\n");
+      }
+   }
 
 }
