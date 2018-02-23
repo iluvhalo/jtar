4e0d7cc5b8f68131b5517005b202dab82d0def65
iluvhalo <hgd145@vols.utk.edu>
2018-02-22 20:25:28 -0500

    update jtar.c

--- jtar.c
+++ jtar.c
@@ -5,9 +5,25 @@
 #include <jval.h>
 #include <jrb.h>
 
-int main (int argc, char **argv) {
+int process_files(char *s);
 
-	printf("1jtar is running\n");
+int main (int argc, char **argv) {
+   int i;
+   char gucci;
+
+   if (argc < 2) {
+      fprintf(stderr, "usage: jtar [cxv] [files ...]\n");
+      exit(1);
+   } 
+
+   gucci = 0;
+   if ((strcmp(argv[1], "c") == 0) || (strcmp(argv[1], "x") == 0) || (strcmp(argv[1], "cv") == 0) || (strcmp(argv[1], "xv") == 0)) {
+      gucci = 1;
+   }
+   if (gucci != 1) {
+      fprintf(stderr, "usage: jtar [cxv] [files ...]\n");
+      exit(1);
+   }
 
 	return 0;
 }
